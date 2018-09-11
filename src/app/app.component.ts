172e53530ca86ad496f2b8b1a80e55e0314e9ec2
Marcus Henning <marcus.henning4@gmail.com>
2018-09-11 19:23:07 +0200

    Dialog-Aufruf eingefügt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { AddUserFormComponent } from './add-user-form/add-user-form.component';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wwi16ama';
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddUserFormComponent, {
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
